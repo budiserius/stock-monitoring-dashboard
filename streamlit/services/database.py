8ea8d4b2301d6474cf99a0ca7d37f81f7d4b4520
budiserius <budiportofolio@gmail.com>
2026-04-08 14:40:12 +0700

    binung ini gimana deploynya

--- streamlit/services/database.py
+++ streamlit/services/database.py
@@ -1,8 +1,9 @@
+import streamlit as st
 from sqlalchemy import create_engine
 import psycopg2
 
-# URL Koneksi Database
-DB_URL = "postgresql+psycopg2://postgres:12345678@localhost:5432/de-stocks"
+db_conf = st.secrets["connections"]["postgresql"]
+DB_URL = f"{db_conf['dialect']}+{db_conf['driver']}://{db_conf['username']}:{db_conf['password']}@{db_conf['host']}:{db_conf['port']}/{db_conf['database']}"
 
 # 1. Definisikan Engine (Untuk SQLAlchemy/Pandas)
 # Ini yang dipanggil oleh from services.database import engine
@@ -13,7 +14,3 @@ engine = create_engine(
     pool_timeout=30,
     pool_recycle=1800
 )
-
-# 2. Fungsi get_connection (Optional, jika masih ada script lama yang butuh psycopg2 mentah)
-def get_connection():
-    return psycopg2.connect("host=host.docker.internal dbname=de-stocks user=postgres password=12345678")
